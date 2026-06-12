a9a8cf93d691dbc0e3988de4066e488cc1dbc93b
Kartik Chavan <chavankartik2004@gmail.com>
2026-06-13 00:10:15 +0530

    Added DSA code

---
+++ prime no.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 14;
+    bool isPrime = true;
+    for (int i = 2; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime)
+        cout << n << " is a prime number.";
+    else
+        cout << n << " is not a prime number.";
+    return 0;
+}
