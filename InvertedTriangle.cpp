e1e0103271c2cefca12614ebcaea9578cc30ed6a
Kartik Chavan <chavankartik2004@gmail.com>
2026-06-16 23:53:56 +0530

    Added inverted triangle pattern

---
+++ InvertedTriangle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    int num = n * (n + 1) / 2; // Total numbers
+
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << num << " ";
+            num--;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
