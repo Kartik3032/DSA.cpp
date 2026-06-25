aa81435ec07a0761667ebab69ce03c6add209ea0
Kartik Chavan <chavankartik2004@gmail.com>
2026-06-25 21:42:10 +0530

    Added array creation

---
+++ Array/Array_creation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    cout << "Elements of the array are: ";
+    cout << arr[0] << endl;
+    cout << arr[1] << endl;
+    cout << arr[2] << endl;
+    cout << arr[3] << endl;
+    cout << arr[4] << endl;
+
+    return 0;
+}
