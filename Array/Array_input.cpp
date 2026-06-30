330c479ad30e0f8b62d14c5cbb61cdfd37c1d37e
Kartik Chavan <chavankartik2004@gmail.com>
2026-06-30 23:42:08 +0530

    Add learning notes

--- Array/Array_input.cpp
+++ Array/Array_input.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 
     int arr[5];
 
-    for(int i = 0; i < 5; i++) {
+    for (int i = 0; i < 5; i++)
+    {
         cin >> arr[i];
     }
 
-    for(int i = 0; i < 5; i++) {
+    for (int i = 0; i < 5; i++)
+    {
         cout << arr[i] << " ";
     }
 }
