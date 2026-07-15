5c55f9f58db6efcfb9ef62ce148710f2db298f3a
Kartik Chavan <chavankartik2004@gmail.com>
2026-07-15 19:05:37 +0530

    Add subarray generation using nested loops

---
+++ Array/subarray_generation.cpp
@@ -0,0 +1,22 @@
+#include <iostram>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5};
+    int n = arr.size();
+
+    for (int st = 0; st < n; st++)
+    {
+        for (int end = st; end < n; end++)
+        {
+            for (int i = st; i <= end; i++)
+            {
+                cout << arr[i];
+            }
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
--- Vector/vector_linearSearch.cpp
+++ Vector/vector_linearSearch.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    int arr[] = {10, 20, 30, 40, 50};
-    int size = 5;
+    vector<int> vec = {10, 20, 30, 40, 50};
+    int size = vec.size();
 
     int target = 30;
     cout << "Enter element to search: ";
     cin >> target;
 
-    for (int val : vec)
-        cout << val << " ";
-}
-return 0;
+    for (int i = 0; i < vec.size(); i++)
+    {
+        if (vec[i] == target)
+        {
+            cout << "Element found at index " << i;
+        }
+    }
+    return 0;
 }
