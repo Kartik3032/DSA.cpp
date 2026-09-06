601f0f23b63fb13747e649f17836a521c324e9f8
Kartik Chavan <chavankartik2004@gmail.com>
2026-09-06 20:39:29 +0530

    Selection Sorting Implementation

--- Sorting/bubble_sort.cpp
+++ Sorting/bubble_sort.cpp
@@ -8,10 +8,10 @@ void bubbleSort(int arr[], int n)
     {
         for (int j = 0; j < n - i - 1; j++)
         {
-            if (arr)[j] > arr[j+1])
-                {
-                    swap(arr[j], arr[j + 1]);
-                }
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
         }
     }
 }
