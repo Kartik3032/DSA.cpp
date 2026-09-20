bc9b387f1639c42960b30f094846c69e0ef95e2d
Kartik Chavan <chavankartik2004@gmail.com>
2026-09-20 20:46:39 +0530

    Solved LeetCode 268 Missing Number using XOR

---
+++ Leetcode/268_Missing_Number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+int missingNumber(vector<int> &nums)
+{
+    int n = nums.size();
+    int ans = n;
+
+    for (int i = 0; i < n; i++)
+    {
+        ans ^= i;
+        ans ^= nums[i];
+    }
+
+    return ans;
+}
